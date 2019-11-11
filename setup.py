8a3f93cd5021ad8f16a244320e4aff1a66770784
Michal Kuk <michal.kuk@frankbold.org>
2019-11-11 17:51:36 +0100

    Package z Docassemblu
    Vyexportovaný package z Docassamblu tak, abychom mohli pracovat na stejných souborech.

--- setup.py
+++ setup.py
@@ -42,10 +42,10 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
                 out.setdefault(package, []).append(prefix+name)
     return out
 
-setup(name='docassemble.ZakladacExperimenty',
+setup(name='docassemble.ZakladacSpolku',
       version='0.0.1',
-      description=('Experimenty k zakladačí spolků'),
-      long_description='# zakladac-spolku\r\nHello world\r\n\r\n\r\n# Požadavky na novou verzi.\r\n- Uložení pouze vybraných informací z interview do tabulky.\r\n- Možnost vrátit se k rozdělanému formuláři.\r\n- Napojení na obchodní rejstřík.\r\n- Napojení na katarstr nemovitostí.\r\n- Pokračování interview po vygenerování dokumentů.\r\n- Checkboxy pro to-do list.\r\n- "Add another" možnost přídat další pole stejného typu (např. zakladatele spolků)\r\n- Custom Api volání do [Ecomailu](https://ecomailczv2.docs.apiary.io/#reference/lists/list-subscribe/add-new-subscriber-to-list).\r\n- Překlad rozhraní do češtiny.\r\n- Přepínací verze fyzická osoba/právnická osoba.\r\n- Získání informací z dříve vyplněného interview\r\n',
+      description=('Průběžná stvaba zakladače spolků'),
+      long_description='# zakladac-spolku\nHello world\n\n\n# Požadavky na novou verzi.\n- Uložení pouze vybraných informací z interview do tabulky.\n- Možnost vrátit se k rozdělanému formuláři.\n- Napojení na obchodní rejstřík.\n- Napojení na katarstr nemovitostí.\n- Pokračování interview po vygenerování dokumentů.\n- Checkboxy pro to-do list.\n- "Add another" možnost přídat další pole stejného typu (např. zakladatele spolků)\n- Custom Api volání do [Ecomailu](https://ecomailczv2.docs.apiary.io/#reference/lists/list-subscribe/add-new-subscriber-to-list).\n- Překlad rozhraní do češtiny.\n- Přepínací verze fyzická osoba/právnická osoba.\n- Získání informací z dříve vyplněného interview\n',
       long_description_content_type='text/markdown',
       author='Michal Kuk',
       author_email='michal.kuk@frankbold.org',
@@ -53,8 +53,8 @@ setup(name='docassemble.ZakladacExperimenty',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=[],
+      install_requires=[''],
       zip_safe=False,
-      package_data=find_package_data(where='docassemble/ZakladacExperimenty/', package='docassemble.ZakladacExperimenty'),
+      package_data=find_package_data(where='docassemble/ZakladacSpolku/', package='docassemble.ZakladacSpolku'),
      )
 
